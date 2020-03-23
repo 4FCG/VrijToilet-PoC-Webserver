3bdc2af92d67d9abec92f69532099e14e559daac
4FCG <30472504+4FCG@users.noreply.github.com>
2020-03-23 17:34:25 +0100

    Metingen CRUD + meting count range filter

--- Webserver-PoC/Webserver-PoC/App_Start/BundleConfig.cs
+++ Webserver-PoC/Webserver-PoC/App_Start/BundleConfig.cs
@@ -9,10 +9,20 @@ namespace Webserver_PoC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/umd/popper.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+              "~/Content/themes/base/jquery-ui.css",
+              "~/Content/themes/base/slider.css",
+              "~/Content/themes/base/datepicker.css",
+              "~/Content/themes/base/theme.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
---
+++ Webserver-PoC/Webserver-PoC/Controllers/MetingenController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Webserver_PoC.Models;
+using X.PagedList;
+
+namespace Webserver_PoC.Controllers
+{
+    public class MetingenController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Metingen
+        public ViewResult Index(string sortOrder, string SearchWord, string MetingFilter, int? page, string currentFilter)
+        {
+            ViewBag.CurrentSort = sortOrder;
+
+            if (SearchWord != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                SearchWord = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = SearchWord;
+
+            var metingen = from s in db.Metings.Include(m => m.Sensor) select s;
+
+            int maxMeting = metingen.Max(m => m.meting_count);
+            ViewBag.Max = maxMeting;
+
+            if (!String.IsNullOrEmpty(SearchWord))
+            {
+                metingen = metingen.Where(s => s.Sensor.name.Contains(SearchWord));
+            }
+
+            if (!String.IsNullOrEmpty(MetingFilter))
+            {
+                int[] range = MetingFilter.Split(new string[] { " - " }, StringSplitOptions.None).Select(s => int.Parse(s)).ToArray();
+                //used these variables due to linq arrayindex error
+                int min = range[0];
+                int max = range[1];
+                metingen = metingen.Where(m => m.meting_count >= min && m.meting_count <= max);
+                ViewBag.ChosenMin = min;
+                ViewBag.ChosenMax = max;
+            }
+            else
+            {
+                ViewBag.ChosenMin = 0;
+                ViewBag.ChosenMax = maxMeting;
+            }
+
+            metingen = metingen.OrderBy(s => s.Sensor.name);
+
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+
+            return View(metingen.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Metingen/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meting meting = db.Metings.Find(id);
+            if (meting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meting);
+        }
+
+        // GET: Metingen/Create
+        public ActionResult Create()
+        {
+            ViewBag.sensor_id = new SelectList(db.Sensors, "sensor_id", "name");
+            return View();
+        }
+
+        // POST: Metingen/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "meting_id,received_timestamp,meting_count,sensor_id")] Meting meting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Metings.Add(meting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.sensor_id = new SelectList(db.Sensors, "sensor_id", "name", meting.sensor_id);
+            return View(meting);
+        }
+
+        // GET: Metingen/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meting meting = db.Metings.Find(id);
+            if (meting == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.sensor_id = new SelectList(db.Sensors, "sensor_id", "name", meting.sensor_id);
+            return View(meting);
+        }
+
+        // POST: Metingen/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "meting_id,received_timestamp,meting_count,sensor_id")] Meting meting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(meting).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.sensor_id = new SelectList(db.Sensors, "sensor_id", "name", meting.sensor_id);
+            return View(meting);
+        }
+
+        // GET: Metingen/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meting meting = db.Metings.Find(id);
+            if (meting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meting);
+        }
+
+        // POST: Metingen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Meting meting = db.Metings.Find(id);
+            db.Metings.Remove(meting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
